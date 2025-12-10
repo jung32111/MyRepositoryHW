a18e58c0d3d3884e3360959e91479701eda08f46
user@tls59050@skuniv.ac.kr <user@tls59050@skuniv.ac.kr>
2025-12-10 09:44:07 +0900

    초기 버전

---
+++ GitHW/src/mygit/B.java
@@ -0,0 +1,10 @@
+package mygit;
+
+public class B {
+
+	public static void main(String[] args) {
+		System.out.println("B 클래스 생성");
+
+	}
+
+}
---
+++ GitHW/src/mygit/C.java
@@ -0,0 +1,10 @@
+package mygit;
+
+public class C {
+
+	public static void main(String[] args) {
+		System.out.println("C 클래스 생성");
+
+	}
+
+}
